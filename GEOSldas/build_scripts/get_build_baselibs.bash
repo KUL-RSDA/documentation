07d0e364f6e700d421f6a836608482f72c6196e8
Michel Bechtold <michel.bechtold@kuleuven.be>
2023-03-01 14:20:45 +0100

    ENH: adjusted for mavx2 use as suggested by Ehsan

--- GEOSldas/build_scripts/get_build_baselibs.bash
+++ GEOSldas/build_scripts/get_build_baselibs.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 #version=6.2.8 # working for tier-1 and tier-2
-version=6.2.13 # working only fier tier-1 currently (texinfo needed for 2021 tier-2 toolchain)
-version=7.7.0 # working only fier tier-1 currently (texinfo needed for 2021 tier-2 toolchain)
+
+# working only fier tier-1 currently (texinfo needed for 2021 tier-2 toolchain)
+version=6.2.13
+
+#version=7.7.0 # working only fier tier-1 currently (texinfo needed for 2021 tier-2 toolchain)
 
 # IMPORTANT: staging is not cross-mounted, so the baselibs are installed at a
 # different location on Tier-1!
@@ -10,7 +13,7 @@ version=7.7.0 # working only fier tier-1 currently (texinfo needed for 2021 tier
 # Load modules and set paths for Tier-1 or Tier-2
 node=`uname -n`
 module purge
-if [[ $node == "tier2"* ]] | [[ $node == "r"* ]]; then 
+if [[ $node == "tier2"* ]] || [[ $node == "r"* ]]; then 
     root=/staging/leuven/stg_00024/GEOSldas_libraries
     if [ -d "${root}/ESMA-Baselibs-v${version}" ]; then
         echo "$root/ESMA-Baselibs-v${version} already exists, first delete ... stopping."
@@ -54,7 +57,8 @@ rm -f ESMA-Baselibs-v${version}.COMPLETE.tar.xz
 cd $root/ESMA-Baselibs-v${version}
 
 # temporary fix since 6.2.13 baselibs were not complete: getting full FLAP from 6.2.8
-if [[ $version == "6.2.13" ]] | [[ $version == "7.7.0" ]] ; then
+if [[ $version == *"6.2.13"* ]] || [[ $version == *"7.7.0"* ]]; then
+    echo "Getting FLAP from 6.2.8"
     rm -r $root/ESMA-Baselibs-v${version}/FLAP
     mkdir $root/temp
     cd $root/temp
@@ -66,9 +70,9 @@ if [[ $version == "6.2.13" ]] | [[ $version == "7.7.0" ]] ; then
     rm -rf $root/temp
 fi
 
-if [[ $version == "6.2.8" ]]; then
+if [[ $version == *"6.2.8"* ]]; then
     cd $root/ESMA-Baselibs-v${version}/src
-elif [[ $version == "6.2.13" ]] | [[ $version == "7.7.0" ]] ; then
+elif [[ $version == *"6.2.13"* ]] || [[ $version == *"7.7.0"* ]]; then
     cd $root/ESMA-Baselibs-v${version}
 fi
 mkdir Linux
@@ -77,13 +81,22 @@ if [[ $node == *"dodrio"* ]]; then
     find Arch.mk -type f -exec sed -i '/Fedora/i # To find tirpc on Hortense' {} \;
     find Arch.mk -type f -exec sed -i '/Fedora/i INC_EXTRA += -I$(EBROOTLIBTIRPC)\/include\/tirpc' {} \;
     find Arch.mk -type f -exec sed -i '/Fedora/i LIB_EXTRA += -ltirpc' {} \;
+    find Arch.mk -type f -exec sed -i '/Fedora/i       FFLAGS += -mavx2 -O2' {} \;
+    find Arch.mk -type f -exec sed -i '/Fedora/i       FCFLAGS += -mavx2 -O2' {} \;
+    find Arch.mk -type f -exec sed -i '/Fedora/i       NAG_FCFLAGS += -mavx2 -O2' {} \;
+    find -name "build_rules.mk" -type f -exec sed -i 's/ESMF_OPTLEVELDEFAULT  = 0/ESMF_OPTLEVELDEFAULT  = 2/g' {} \;
+    find -name "makefile" -type f -exec sed -i 's/ESMF_OPTLEVEL = 0/ESMF_OPTLEVEL = 2/g' {} \;
+    find -name "*" -type f -exec sed -i 's/-fallow-argument-mismatch -O/-fallow-argument-mismatch/g' {} \;
 fi
 
 # Build Baselibs
 export FC=gfortran
-if [[ $version == "6.2.8" ]]; then
+echo $version
+if [[ $version == *"6.2.8"* ]]; then
+    echo "Compiling ..................."
     make install ESMF_COMM=openmpi prefix=$root/ESMA-Baselibs-v${version}/src/Linux
-elif [[ $version == "6.2.13" ]] | [[ $version == "7.7.0" ]]; then
+elif [[ $version == *"6.2.13"* ]] || [[ $version == *"7.7.0"* ]]; then
+    echo "Compiling ..................."
     make install ESMF_COMM=openmpi prefix=$root/ESMA-Baselibs-v${version}/Linux
 fi
 
